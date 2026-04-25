a78a16382738419869614e55deb8d11aeac14f94
jozelot_ <jozelot77@gmail.com>
2026-04-25 21:23:54 +0200

    feat: plugin test instance

---
+++ plugin/src/test/java/de/jozelot/jmanhunt/utility/TimerTest.java
@@ -0,0 +1,12 @@
+package de.jozelot.jmanhunt.utility;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TimerTest {
+
+    @Test
+    void testTimeFormatting() {
+
+    }
+}
