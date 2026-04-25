c6d388792d06db00a30f0c4d8169f42754db348f
jozelot_ <jozelot77@gmail.com>
2026-04-26 01:21:40 +0200

    fix: backbutton is now in the inventory and not 1 slot outside

--- plugin/src/main/java/de/jozelot/jmanhunt/inventory/menu/CompassSelector.java
+++ plugin/src/main/java/de/jozelot/jmanhunt/inventory/menu/CompassSelector.java
@@ -43,7 +43,7 @@ public class CompassSelector extends Menu{
             setPlayer(runner, i + 9,player.getTracking().get() == runner );
         }
         setFiller(inventory.getSize());
-        setBackButton(inventory.getSize(), player, null);
+        setBackButton(inventory.getSize() - 1, player, null);
     }
 
     private void setPlayer(ManhuntPlayer runner, int slot, boolean isCurrentTarget) {
