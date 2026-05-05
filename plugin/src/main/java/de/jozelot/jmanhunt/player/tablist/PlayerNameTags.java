eef0b034886d81ebf6da6bfa34bdfad52925c009
jozelot_ <jozelot77@gmail.com>
2026-05-05 19:08:38 +0200

    feat: added custom name tag

--- plugin/src/main/java/de/jozelot/jmanhunt/player/tablist/PlayerNameTags.java
+++ plugin/src/main/java/de/jozelot/jmanhunt/player/tablist/PlayerNameTags.java
@@ -1,4 +1,60 @@
 package de.jozelot.jmanhunt.player.tablist;
 
+import de.jozelot.jmanhunt.JManhunt;
+import de.jozelot.jmanhunt.api.player.ManhuntPlayer;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.Team;
+
 public class PlayerNameTags {
+
+    private final JManhunt plugin;
+
+    public PlayerNameTags(JManhunt plugin) {
+        this.plugin = plugin;
+    }
+
+    public void updateNameTag(ManhuntPlayer player) {
+        Scoreboard board = plugin.getServer().getScoreboardManager().getMainScoreboard();
+        String teamId = "tm_" + player.getPlayer().getName();
+        Team team = board.getTeam(teamId);
+
+        if (team == null) {
+            team = board.registerNewTeam(teamId);
+        }
+
+        var config = plugin.getBootstrap().getConfigManager();
+        var teamConf = config.getTeam();
+
+        String teamColor = switch (player.getTeam()) {
+            case HUNTER -> teamConf.getHunter().getColor();
+            case RUNNER -> teamConf.getRunner().getColor();
+            case SPECTATOR -> teamConf.getSpectator().getColor();
+            case NONE -> teamConf.getNone().getColor();
+        };
+
+        String teamDisplayName = teamColor + plugin.getBootstrap().getLangManager().format("teams." + player.getTeam().name().toLowerCase(), null);
+
+        String format = config.getTeamPrefix().getFormat();
+        String[] parts = format.split("\\{player_name}");
+
+        if (parts.length > 0) {
+            String prefixRaw = parts[0].replace("{team}", teamDisplayName);
+            team.prefix(MiniMessage.miniMessage().deserialize(prefixRaw));
+        } else {
+            team.prefix(Component.empty());
+        }
+
+        if (parts.length > 1) {
+            String suffixRaw = parts[1].replace("{team}", teamDisplayName);
+            team.suffix(MiniMessage.miniMessage().deserialize(suffixRaw));
+        } else {
+            team.suffix(Component.empty());
+        }
+
+        if (!team.hasEntry(player.getPlayer().getName())) {
+            team.addEntry(player.getPlayer().getName());
+        }
+    }
 }
