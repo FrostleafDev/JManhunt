c33e11c58f26f9b0533b09d4fb938d3e43d3989a
jozelot_ <jozelot77@gmail.com>
2026-05-07 17:31:33 +0200

    feat(placholders): added timer and distance to runner placeholders

--- plugin/src/main/java/de/jozelot/jmanhunt/core/dependencies/ManhuntPlaceholders.java
+++ plugin/src/main/java/de/jozelot/jmanhunt/core/dependencies/ManhuntPlaceholders.java
@@ -1,11 +1,16 @@
 package de.jozelot.jmanhunt.core.dependencies;
 
 import de.jozelot.jmanhunt.JManhunt;
+import de.jozelot.jmanhunt.api.game.timer.ManhuntTimer;
+import de.jozelot.jmanhunt.api.player.ManhuntPlayer;
 import de.jozelot.jmanhunt.player.ManhuntPlayerImpl;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Optional;
 
 public class ManhuntPlaceholders extends PlaceholderExpansion {
 
@@ -69,6 +74,59 @@ public class ManhuntPlaceholders extends PlaceholderExpansion {
         // %jmanhunt_total_spectators%
         if (params.equalsIgnoreCase("total_spectators")) {
             return String.valueOf(bootstrap.getManhuntPlayerManager().getSpectators().size());
+        }
+
+        // %jmanhunt_timer_%
+        if (params.startsWith("timer_")) {
+            String timeOption = params.replace("timer_", "");
+            ManhuntTimer timer = plugin.getBootstrap().getTimerManager().getTimer();
+            long totalSeconds = timer.getElapsedSeconds();
+
+            long d = totalSeconds / 86400;
+            long h = (totalSeconds % 86400) / 3600;
+            long m = (totalSeconds % 3600) / 60;
+            long s = totalSeconds % 60;
+
+            return switch (timeOption) {
+                case "s" -> String.valueOf(s);
+                case "S" -> String.format("%02d", s);
+                case "m" -> String.valueOf(m);
+                case "M" -> String.format("%02d", m);
+                case "h" -> String.valueOf(h);
+                case "H" -> String.format("%02d", h);
+                case "d" -> String.valueOf(d);
+                case "D" -> String.format("%02d", d);
+                case "formatted" -> (h > 0)
+                        ? String.format("%02d:%02d:%02d", h, m, s)
+                        : String.format("%02d:%02d", m, s);
+                default -> null;
+            };
+        }
+
+        // %jmanhunt_runner_distance%
+        if (params.startsWith("runner_distance")) {
+            Optional<ManhuntPlayer> trackingOpt = manhuntPlayer.getTracking();
+
+            if (trackingOpt.isEmpty()) return "0";
+
+            ManhuntPlayer tracking = trackingOpt.get();
+
+            if (!manhuntPlayer.isOnline() || !tracking.isOnline()) return "0";
+
+            Player hunter = manhuntPlayer.getPlayer();
+            Player runner = tracking.getPlayer();
+
+            if (!hunter.getWorld().equals(runner.getWorld())) {
+                return plugin.getBootstrap().getLangManager().format("different-dimension", null);
+            }
+
+            double distance = hunter.getLocation().distance(runner.getLocation());
+
+            if (params.endsWith("_int")) {
+                return String.valueOf((int) distance);
+            }
+
+            return String.format("%.1f", distance);
         }
 
         // TODO: goal, goal_progress
